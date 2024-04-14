fb4fa747e8d6ed985ef509abbcd807b9adddcd98
mandal10 <133206937+mandal10@users.noreply.github.com>
2024-04-14 09:40:23 -0700

    Add files via upload

---
+++ M4/W2/D2/modules/module1.js
@@ -0,0 +1,10 @@
+const name="soumya"
+const age= 20
+const ar=[1,3,4,5]
+
+export {name,ar,age}
+
+
+// named export= we can export any number of functions, variables.
+
+// default export= We can have only one default export in a file.
---
+++ M4/W2/D2/modules/usemodule.js
@@ -0,0 +1,9 @@
+import {name, age} from './module1.js'
+
+function message(){
+
+    return `Hello ${name}, you are ${age} years old`;
+}
+
+
+export default message;
